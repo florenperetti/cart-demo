aea8deb8afa2fa7bc9ce03a557874e0bdfb0f0ba
Floren Peretti <floren.peretti@knowbly.com>
2019-11-24 15:00:33 -0300

    updated products

---
+++ dist/precache-manifest.d112645b6c8754eaf608594422ae5ed7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1086647e596fcd0c3f7e",
+    "url": "/cart-demo/css/about.8e70155f.css"
+  },
+  {
+    "revision": "0c781a3a580141265db3",
+    "url": "/cart-demo/css/app.bb3dc608.css"
+  },
+  {
+    "revision": "993ab968f4c237596b70640bb3a5aff4",
+    "url": "/cart-demo/img/camera-rec.993ab968.jpg"
+  },
+  {
+    "revision": "c11c5f5a4fb095a89bb6d28df6248ebb",
+    "url": "/cart-demo/img/camera-stand.c11c5f5a.jpg"
+  },
+  {
+    "revision": "c907bfd0aea068f01487e49863076231",
+    "url": "/cart-demo/img/camera.c907bfd0.jpg"
+  },
+  {
+    "revision": "1e19caed4464666bf1fe47a594db871d",
+    "url": "/cart-demo/img/gameboy.1e19caed.jpg"
+  },
+  {
+    "revision": "c2c78cf0f8b4935bd9be9e67db6dcde3",
+    "url": "/cart-demo/img/keyboard.c2c78cf0.jpg"
+  },
+  {
+    "revision": "eb2c6b42828aed622f8962c56018eee6",
+    "url": "/cart-demo/img/logo.eb2c6b42.png"
+  },
+  {
+    "revision": "03ceec7d4b4008a87e0f3f3c989f3714",
+    "url": "/cart-demo/img/mouse.03ceec7d.jpg"
+  },
+  {
+    "revision": "80da124668287b9c986062209b14ca40",
+    "url": "/cart-demo/img/nes-controler.80da1246.jpg"
+  },
+  {
+    "revision": "1d1e57b3f2113cce5f5057de1c706ea5",
+    "url": "/cart-demo/img/phone.1d1e57b3.jpg"
+  },
+  {
+    "revision": "209d48861eb50eaf3011cb976592c9f9",
+    "url": "/cart-demo/img/plasma-tv.209d4886.jpg"
+  },
+  {
+    "revision": "b6c4946cd248012a18aed0d303627352",
+    "url": "/cart-demo/img/radio.b6c4946c.jpg"
+  },
+  {
+    "revision": "4aa205702016300cdd53b962cfd457d2",
+    "url": "/cart-demo/img/remote.4aa20570.jpg"
+  },
+  {
+    "revision": "2fc42e5bd06b5bc3353b099d2ef8acde",
+    "url": "/cart-demo/img/telephone.2fc42e5b.jpg"
+  },
+  {
+    "revision": "87939ac66d655c3e54a01e98deddf235",
+    "url": "/cart-demo/index.html"
+  },
+  {
+    "revision": "1086647e596fcd0c3f7e",
+    "url": "/cart-demo/js/about.9878b58c.js"
+  },
+  {
+    "revision": "0c781a3a580141265db3",
+    "url": "/cart-demo/js/app.70f56011.js"
+  },
+  {
+    "revision": "fbeb87aa6808dbeceb69",
+    "url": "/cart-demo/js/chunk-vendors.ffde4f35.js"
+  },
+  {
+    "revision": "c86ad742a49c745e6c55797332cdd617",
+    "url": "/cart-demo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cart-demo/robots.txt"
+  }
+]);
